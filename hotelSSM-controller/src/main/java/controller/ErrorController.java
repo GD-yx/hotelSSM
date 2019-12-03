1b2e8d2f83f9ea6460935173c0fb439da8a6f750
GD <1841550221@qq.com>
2019-12-03 19:06:16 +0800

    酒店管理

--- hotelSSM-controller/src/main/java/controller/ErrorController.java
+++ hotelSSM-controller/src/main/java/controller/ErrorController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
- * @author 南八
+ * @author 南八苟富贵
  */
 @Controller
 public class ErrorController {
